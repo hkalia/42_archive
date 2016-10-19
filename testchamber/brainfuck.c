ea31249cd6e58e0d2d47488aba7b41bf51558e34
hkalia <habibkalia@gmail.com>
2016-10-19 12:20:56 -0700

    testchamber

--- testchamber/brainfuck.c
+++ testchamber/brainfuck.c
@@ -6,102 +6,69 @@
 /*   By: hkalia <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/10 13:46:46 by hkalia            #+#    #+#             */
-/*   Updated: 2016/10/10 15:32:28 by hkalia           ###   ########.fr       */
+/*   Updated: 2016/10/19 12:17:25 by hkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
 
-unsigned char	*ft_memnew(int ret_len)
+#define MEM_SIZE 2048
+
+char mem[MEM_SIZE] = {0};
+char *memp = mem;
+
+void	brainfuck(char *src)
 {
-	unsigned char	*ret;
-	int				i;
-	
-	i = 0;
-	if (!(ret = (unsigned char *)malloc(sizeof(unsigned char) * (ret_len + 1))))
-		return (0);
-	while (i < ret_len)
-	{
-		ret[i] = 0;
-		i++;
-	}
-	ret[i] = 0;
-	return (ret);
-}
+	int 	i;
+	int 	loop;
 
-int				brainfuck(char *src, int src_len)
-{
-	unsigned char	*mem;
-	unsigned char	cur;
-	int				i;
-	int				loop;
-
-	if (!(mem = ft_memnew(2048)))
-		return (1);
 	i = 0;
-	while (i < src_len)
+	while (src[i] != 0)
 	{
-		switch (src[i])
+		if (src[i] == '>')
+			++memp;
+		else if (src[i] == '<')
+			--memp;
+		else if (src[i] == '+')
+			++*memp;
+		else if (src[i] == '-')
+			--*memp;
+		else if (src[i] == '.')
+			write(1, memp, 1);
+		else if (src[i] == '[' && *memp == 0)
 		{
-			case '>' :
-				mem++;
-				break ;
-			case '<' :
-				mem--;
-				break ;
-			case '+' :
-				(*mem)++;
-				break ;
-			case '-' :
-				(*mem)--;
-				break ;
-			case '.' :
-				write(1, mem, 1);
-				break ;
-			case '[' :
-				break ;
-			case ']' :
-				if (*mem)
-				{
-					loop = 1;
-					while (loop > 0)
-					{
-						cur = src[--i];
-						if (cur == '[')
-							loop--;
-						else if (cur == ']')
-							loop++;
-					}
-				}
-				break ;
+			loop = 1;
+			while (loop != 0)
+			{
+				++i;
+				if (src[i] == '[')
+					++loop;
+				else if (src[i] == ']')
+					--loop;
+			}
 		}
-		i++;
+		else if (src[i] == ']' && *memp != 0)
+		{
+			loop = 1;
+			while (loop != 0)
+			{
+				--i;
+				if (src[i] == ']')
+					++loop;
+				else if (src[i] == '[')
+					--loop;
+			}
+		}
+		++i;
 	}
-	return (0);
-}
-
-int				ft_strlen(char *src)
-{
-	int		len;
-	
-	len = 0;
-	while (src[len])
-		len++;
-	return (len);
 }
 
 int		main(int argc, char **argv)
 {
 	if (argc == 2)
-	{
-		if (brainfuck(argv[1], ft_strlen(argv[1])))
-		{
-			write(1, "MALLOC ERROR\n", 13);
-			return (1);
-		}
-	}
+		brainfuck(argv[1]);
 	else
 		write(1, "\n", 1);
-	return (0);
+	return 0;
 }
