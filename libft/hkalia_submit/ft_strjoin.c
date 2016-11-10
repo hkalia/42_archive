4a85ef9084a9d0effcfc59afe788205615676919
hkalia <habibkalia@gmail.com>
2016-11-09 16:49:24 -0800

    fixing submission libft

--- libft/hkalia_submit/ft_strjoin.c
+++ libft/hkalia_submit/ft_strjoin.c
@@ -3,15 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hkalia <hkalia@student.42.us.org>          +#+  +:+       +#+        */
+/*   By: hkalia <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/22 15:39:44 by hkalia            #+#    #+#             */
-/*   Updated: 2016/11/08 15:55:04 by hkalia           ###   ########.fr       */
+/*   Updated: 2016/09/28 08:53:00 by hkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <ft_string.h>
-#include <ft_custom.h>
+#include <string.h>
+#include <stdlib.h>
 
 char	*ft_strjoin(const char *src1, const char *src2)
 {
@@ -20,11 +20,15 @@ char	*ft_strjoin(const char *src1, const char *src2)
 	size_t	src2_len;
 	size_t	i;
 
-	src1_len = ft_strlen(src1);
-	src2_len = ft_strlen(src2);
+	src1_len = 0;
+	src2_len = 0;
 	i = -1;
-	if (!(dst = ft_strnew(src1_len + src2_len))))
-		return (0);
+	while (src1[src1_len])
+		src1_len++;
+	while (src2[src2_len])
+		src2_len++;
+	if (!(dst = (char*)malloc(sizeof(char) * (src1_len + src2_len))))
+		return (NULL);
 	while (++i < src1_len)
 		dst[i] = src1[i];
 	i = 0;
