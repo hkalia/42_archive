4d1befd3911d333d859e5eadebc3d782a8c358ce
Habib Kalia <habibkalia@gmail.com>
2016-12-06 10:48:25 -0800

    deleted old arr

--- libft/hkalia_current/test.c
+++ libft/hkalia_current/test.c
@@ -6,19 +6,27 @@
 /*   By: hkalia <hkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/02 16:52:57 by hkalia            #+#    #+#             */
-/*   Updated: 2016/12/03 09:31:46 by hkalia           ###   ########.fr       */
+/*   Updated: 2016/12/06 10:42:53 by hkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 #include <stdio.h>
-#include <ctype.h>
 
-int		main(void)
-{
-	char str[20000];
+int main() {
+  UT_array *nums;
+  int i, *p;
 
-	ft_bzero(&str, sizeof(str));
-	printf("%s\n", str);
-	return 0;
+  utarray_new(nums,&ut_int_icd);
+  for(i=0; i < 10; i++) utarray_push_back(nums,&i);
+
+  for(p=(int*)utarray_front(nums);
+      p!=NULL;
+      p=(int*)utarray_next(nums,p)) {
+    printf("%d\n",*p);
+  }
+
+  utarray_free(nums);
+
+  return 0;
 }
