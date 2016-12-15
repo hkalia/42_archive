afc6c1b1daf761d6ddebeb169c4e2d7bcd2f3ed5
Habib Kalia <habibkalia@gmail.com>
2016-12-14 20:05:45 -0800

    libft

--- libft/hkalia_current/test.c
+++ libft/hkalia_current/test.c
@@ -6,7 +6,7 @@
 /*   By: hkalia <hkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/02 16:52:57 by hkalia            #+#    #+#             */
-/*   Updated: 2016/12/14 19:42:51 by hkalia           ###   ########.fr       */
+/*   Updated: 2016/12/14 19:58:11 by hkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,33 +19,38 @@ int		main(void)
 {
 	t_arr	test;
 
-	arr_insertat(&test, 0, "hello", 5);
+	GRD(arr_insertat(&test, 0, "hello", 5) == -1, -1);
 	ft_putnbr(test.len);
 	write(1, " ", 1);
 	ft_putnbr(test.cap);
 	write(1, " ", 1);
 	write(1, test.arr, test.len);
 	write(1, "\n", 1);
-	arr_insertat(&test, 0, "smerch", 6);
+
+	GRD(arr_insertat(&test, 0, "smerch", 6) == -1, -1);
 	ft_putnbr(test.len);
 	write(1, " ", 1);
 	ft_putnbr(test.cap);
 	write(1, " ", 1);
 	write(1, test.arr, test.len);
 	write(1, "\n", 1);
-	arr_insertat(&test, 100, "word", 4);
+
+	GRD(arr_insertat(&test, 100, "word", 4) == -1, -1);
 	ft_putnbr(test.len);
 	write(1, " ", 1);
 	ft_putnbr(test.cap);
 	write(1, " ", 1);
 	write(1, test.arr, test.len);
 	write(1, "\n", 1);
-	arr_removeat(&test, 11, 89);
+
+	GRD(arr_removeat(&test, 11, 89) == -1, -1);
 	ft_putnbr(test.len);
 	write(1, " ", 1);
 	ft_putnbr(test.cap);
 	write(1, " ", 1);
 	write(1, test.arr, test.len);
 	write(1, "\n", 1);
+
+	arr_dtr(&test);
 	return (0);
 }
