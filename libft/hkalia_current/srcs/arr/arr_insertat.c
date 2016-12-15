3f79e792d87b07f6dfc8f086517200c88a57eff2
Habib Kalia <habibkalia@gmail.com>
2016-12-14 19:06:17 -0800

    libft

--- libft/hkalia_current/srcs/arr/arr_insertat.c
+++ libft/hkalia_current/srcs/arr/arr_insertat.c
@@ -6,7 +6,7 @@
 /*   By: hkalia <hkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/12 13:08:42 by hkalia            #+#    #+#             */
-/*   Updated: 2016/12/14 17:37:13 by hkalia           ###   ########.fr       */
+/*   Updated: 2016/12/14 18:16:41 by hkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,20 @@
 
 int8_t	arr_insertat(t_arr *dst, size_t i, void *src, size_t src_len)
 {
-	size_t	j;
-
 	GRD(dst == 0, -1);
 	GRD(src == 0 || src_len == 0, 0);
-	j = i > dst->cap ? (i - dst->cap) + src_len : src_len;
-	GRD(arr_reserve(dst, j) == -1, -1);
+	GRD(arr_reserve(dst
+		, i > dst->cap ? (i - dst->cap) + src_len : src_len) == -1, -1);
 	if (dst->len > i)
 	{
 		ft_memmove(&dst->arr[i + src_len], &dst->arr[i], dst->len - i);
 		ft_memcpy(&dst->arr[i], src, src_len);
+		dst->len += src_len;
 	}
 	else
+	{
 		ft_memcpy(&dst->arr[i], src, src_len);
-	dst->len += j;
+		dst->len = i + src_len;
+	}
 	return (0);
 }
--- libft/hkalia_current/test.c
+++ libft/hkalia_current/test.c
@@ -6,7 +6,7 @@
 /*   By: hkalia <hkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/02 16:52:57 by hkalia            #+#    #+#             */
-/*   Updated: 2016/12/14 17:42:14 by hkalia           ###   ########.fr       */
+/*   Updated: 2016/12/14 18:12:40 by hkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,25 @@ int		main(void)
 	t_arr	test;
 
 	arr_insertat(&test, 0, "hello", 5);
-	arr_insertat(&test, 2, "hello", 5);
-	arr_insertat(&test, 100, "hello", 5);
 	ft_putnbr(test.len);
 	write(1, " ", 1);
 	ft_putnbr(test.cap);
 	write(1, " ", 1);
 	write(1, test.arr, test.len);
+	write(1, "\n", 1);
+	arr_insertat(&test, 0, "smerch", 6);
+	ft_putnbr(test.len);
+	write(1, " ", 1);
+	ft_putnbr(test.cap);
+	write(1, " ", 1);
+	write(1, test.arr, test.len);
+	write(1, "\n", 1);
+	arr_insertat(&test, 100, "word", 4);
+	ft_putnbr(test.len);
+	write(1, " ", 1);
+	ft_putnbr(test.cap);
+	write(1, " ", 1);
+	write(1, test.arr, test.len);
+	write(1, "\n", 1);
 	return (0);
 }
