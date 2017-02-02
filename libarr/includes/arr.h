ff1b767e3d5dabc150e3b86776c1884d94e23a02
Habib Kalia <habibkalia@gmail.com>
2017-02-02 11:35:18 -0800

    arr

--- libarr/includes/arr.h
+++ libarr/includes/arr.h
@@ -6,7 +6,7 @@
 /*   By: hkalia <hkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/02 17:03:28 by hkalia            #+#    #+#             */
-/*   Updated: 2017/01/28 17:41:32 by hkalia           ###   ########.fr       */
+/*   Updated: 2017/02/02 10:09:14 by hkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,10 @@
 
 typedef struct	s_elm
 {
-	size_t		sze;
-	int8_t		(*zero)(void *elm);
-	int8_t		(*dup)(void *dst, const void *src);
-	void		(*dtr)(void *elm);
+	size_t	sze;
+	int8_t	(*zero)(void *elm);
+	int8_t	(*dup)(void *dst, const void *src);
+	void	(*dtr)(void *elm);
 }				t_arr_elm;
 
 typedef struct	s_arr
@@ -53,12 +53,16 @@ int8_t			arr_insert(t_arr *dst, size_t idx, const void *src);
 int8_t			arr_insertm(t_arr *dst, size_t idx, const void *src
 							, size_t src_len);
 int8_t			arr_insertarr(t_arr *dst, size_t idx, t_arr *src);
+int8_t			arr_append(t_arr *dst, const void *src);
+int8_t			arr_appendm(t_arr *dst, const void *src, size_t src_len);
+int8_t			arr_appendarr(t_arr *dst, t_arr *src);
 void			arr_swap(t_arr *src, size_t i, size_t j);
 void			arr_reverse(t_arr *src);
 void			arr_qsort(t_arr *src, int (*cmp)(const void *, const void *
 							, size_t));
 void			arr_qsort_r(t_arr *src, void *thunk, int (*cmp)(const void *
 							, const void *, size_t, void *));
+int8_t			arr_remove(t_arr *src, size_t idx);
 int8_t			arr_removem(t_arr *src, size_t idx, size_t len);
 char			*arr_tostr(t_arr *src);
 
